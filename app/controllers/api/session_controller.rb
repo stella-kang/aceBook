6efb89b25b65c922433c3eb5b61c15af4290437c
stella-kang <stella.kang13@gmail.com>
2021-10-29 12:21:03 -0400

    Add session controller

--- app/controllers/api/session_controller.rb
+++ app/controllers/api/session_controller.rb
@@ -1,9 +1,23 @@
 class Api::SessionController < ApplicationRecord
     def create
+        @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
 
+        if @user
+            login(@user)
+            render '/index'
+        else
+            render json: ["Invalid login credentials"], status: 401
+        end
     end
 
     def destroy
+        @user = current_user
 
+        if @user
+            logout!
+            render json: {}
+        else
+            render json: ["Must be logged in"], status: 404
+        end
     end
 end
