eb89ea56294eaa7998f5ea53c612f1f23018f48d
stella-kang <stella.kang13@gmail.com>
2021-11-08 11:54:35 -0500

    AAdd esc key listener to edit comments

--- frontend/components/comments/comment_form.jsx
+++ frontend/components/comments/comment_form.jsx
@@ -10,6 +10,23 @@ export default class CommentForm extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    componentDidMount() {
+        if (this.props.formType === 'edit') {
+            const commentForms = document.querySelectorAll(".comment-edit-form")
+            commentForms.forEach(form => {
+                form.addEventListener("keydown", (e) => {
+                    if (e.key === 'Escape') {
+                        form.style.display = '';
+                        const commentItem = document.getElementById(`comment-${this.props.comment.id}`);
+                        if (commentItem) {
+                            commentItem.style.display = "flex";
+                        }
+                    }
+                })
+            })
+        }
+    }
+
     update(e) {
         this.setState({ "content": e.target.value })
     }
@@ -38,9 +55,10 @@ export default class CommentForm extends React.Component {
     }
 
     render() {
-        return <form>
+        return <form id='comment-form'>
             <input id={`comment-input-${this.props.comment.post_id}-${this.props.comment.id}`} type="text" onChange={this.update} placeholder="Write a comment..." value={this.state.content}/>
             <button id="create-comment-button" onClick={this.handleSubmit}></button>
+            {this.props.formType === "edit" ? <div>Press Esc to cancel.</div> : null}
         </form>
     }
 }
